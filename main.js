1d233f775ef36540d8961a813279a519605a20fc
Luan Cardoso <luanleitecardoso@gmail.com>
2021-07-09 12:48:20 -0300

    adding javascript files

---
+++ main.js
@@ -0,0 +1,46 @@
+function getTodos() {
+  console.log('Get Request')
+}
+
+function addTodo() {
+  console.log('addTodo Request')
+}
+
+function updateTodo() {
+  console.log('updateTodo Request')
+}
+
+function removeTodo() {
+  console.log('removeTodo Request')
+}
+
+function getData() {
+  console.log('getData Request')
+}
+
+function customHeaders() {
+  console.log('customHeaders Request')
+}
+
+function transformResponse() {
+  console.log('transformResponse Request')
+}
+
+function errorHandling() {
+  console.log('errorHandling Request')
+}
+
+function cancelToken() {
+  console.log('cancelToken Request')
+}
+
+// Event listeners
+document.getElementById('get').addEventListener('click', getTodos);
+document.getElementById('post').addEventListener('click', addTodo);
+document.getElementById('update').addEventListener('click', updateTodo);
+document.getElementById('delete').addEventListener('click', removeTodo);
+document.getElementById('sim').addEventListener('click', getData);
+document.getElementById('headers').addEventListener('click', customHeaders);
+document.getElementById('transform').addEventListener('click', transformResponse);
+document.getElementById('error').addEventListener('click', errorHandling);
+document.getElementById('cancel').addEventListener('click', cancelToken);
