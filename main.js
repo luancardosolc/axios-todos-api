08366ed9b7bca56c4e09fb7fc7de35984e32ca22
Luan Cardoso <luanleitecardoso@gmail.com>
2021-07-10 08:28:32 -0300

    Formating response as output in the browser

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function getTodos() {
     method: 'get',
     url: 'https://jsonplaceholder.typicode.com/todos'
   })
-    .then(res => console.log(res))
+    .then(res => showOutput(res))
     .catch(err => console.error(err)) 
 }
 
@@ -37,6 +37,39 @@ function errorHandling() {
 
 function cancelToken() {
   console.log('cancelToken Request')
+}
+
+//Formatting output in browser
+function showOutput(res) {
+  document.getElementById('res').innerHTML = `
+  <div class="card card-body mb-4">
+    <h5>Status: ${res.status}</h5>
+  </div>
+  <div class="card mt-3">
+    <div class="card-header">
+      Headers
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.headers, null, 2)}</pre>
+    </div>
+  </div>
+  <div class="card mt-3">
+    <div class="card-header">
+      Data
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.data, null, 2)}</pre>
+    </div>
+  </div>
+  <div class="card mt-3">
+    <div class="card-header">
+      Config
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.config, null, 2)}</pre>
+    </div>
+  </div>
+`;
 }
 
 // Event listeners
