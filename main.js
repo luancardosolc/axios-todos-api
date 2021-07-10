e3c969967427b4f5b2db35c142b1e7733d47b838
Luan Cardoso <luanleitecardoso@gmail.com>
2021-07-10 08:38:36 -0300

    Axios.get in a shorter way

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 function getTodos() {
-  axios({
-    method: 'get',
-    url: 'https://jsonplaceholder.typicode.com/todos',
+  // axios({
+  //   method: 'get',
+  //   url: 'https://jsonplaceholder.typicode.com/todos',
+  //   params: {
+  //     _limit: 5
+  //   }
+  // })
+  //   .then(res => showOutput(res))
+  //   .catch(err => console.error(err)) 
+
+  axios.get('https://jsonplaceholder.typicode.com/todos', {
     params: {
       _limit: 5
     }
