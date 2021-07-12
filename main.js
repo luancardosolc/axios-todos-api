4feecb85dbd009f2ebf735568f9b198158c8aba4
Luan Cardoso <luanleitecardoso@gmail.com>
2021-07-11 21:36:39 -0300

    Global header configs

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// Axios Global Settings
+axios.defaults.headers.common['X-Auth-Token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+
 function getTodos() {
   //--Long way
   // axios({
